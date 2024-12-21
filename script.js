753285e9bcd8e6c84309e1ab82c64ddb4ba19228
spookywookie73 <110636973+spookywookie73@users.noreply.github.com>
2024-12-21 21:44:12 +0000

    created lists of words and random number generator

---
+++ script.js
@@ -0,0 +1,8 @@
+randomWords1 = ['neighbourhood', 'town', 'city', 'local supermaket'];
+randomWords2 = ['giant ants', 'zombies', 'aliens', 'inflatable ponies'];
+randomWords3 = ['the airforce', 'heavy artillery', 'emergency services', 'cannon fodder'];
+randomWords4 = ['very high', 'good', 'not so good', 'slim to none'];
+
+// create a variable that randomly chooses a number between 0 and 3.
+const randomNumber = Math.floor(Math.random() * 4);
+
