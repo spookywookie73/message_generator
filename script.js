448c7efcf3957e11384c403d2616eae75af358a0
spookywookie73 <110636973+spookywookie73@users.noreply.github.com>
2024-12-21 22:21:46 +0000

    tested and cleaned up code

--- script.js
+++ script.js
@@ -3,11 +3,21 @@ randomWords2 = ['giant ants', 'zombies', 'aliens', 'inflatable ponies'];
 randomWords3 = ['the airforce', 'heavy artillery', 'emergency services', 'cannon fodder'];
 randomWords4 = ['very high', 'good', 'not so good', 'slim to none'];
 
-// create a variable that randomly chooses a number between 0 and 3.
-const randomNumber = Math.floor(Math.random() * 4);
+const messageGenerator = () => {
+  const randomNumber = () => Math.floor(Math.random() * 4);
+  
+  console.log(`\n!Urgent message!, Your ${randomWords1[randomNumber()]} is under a red alert.\n`);
+  
+  console.log(`Satellites have detected ${randomWords2[randomNumber()]} preparing to attack.\n`);
+  
+  const chosenNum = randomNumber();
+  if (chosenNum != 3) {
+    console.log(`You have been randomly chosen to help ${randomWords3[chosenNum]}.\n`);
+  } else {
+    console.log(`You have been chosen to be ${randomWords3[chosenNum]}.\n`)
+  };
 
-const sentenceOne = () => {
-  console.log(`!Urgent message!, Your ${randomWords1[randomNumber]} is under a red alert.`);
+  console.log(`The odds of your success are ${randomWords4[randomNumber()]}. Good Luck.\n`);
 };
 
-sentenceOne();
+messageGenerator()
